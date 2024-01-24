6cda8ede4404423d37bc1c07844e821204747632
Nalla Keerthi <kumars963@yahoo.com>
2024-01-24 15:34:13 -0800

    sweet alert

--- ShoppingCart/wwwroot/js/Company.js
+++ ShoppingCart/wwwroot/js/Company.js
@@ -17,21 +17,21 @@
                 "render": function (data) {
                     return `<div>
                         <div class="m-75 btn-group">
-						<a href="/Company/UpsertCompany/${data}" 
-					   class="btn btn-primary mx-2">
-							<i class="bi bi-pencil-square"></i>Edit
-						</a>
+<a href="/Company/UpsertCompany/${data}"
+  class="btn btn-primary mx-2">
+<i class="bi bi-pencil-square"></i>Edit
+</a>
 
-                      <a href="/company/Delete/${data}" onclick="myFunction(data); return false;"
+                      <a href="/company/Delete/${data}" onclick="return myFunction();"
 
-					   class="btn btn-danger mx-2">
-							<i class="bi bi-trash-fill"></i>Delete
-						</a>
-					</div>
+  class="btn btn-danger mx-2">
+<i class="bi bi-trash-fill"></i>Delete
+</a>
+</div>
                             </div>
                             <script>
-                          function myFunction(data) {
-                            
+                          function myFunction() {
+                           
                                     Swal.fire({
                 title: 'Are you sure?',
                 text: "You won't be able to revert this!",
@@ -40,15 +40,47 @@
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire(
-                        'Deleted!',
-                        'Your file has been deleted.',
-                        'success'
-                    )
-                }
-            })
+            }).then((result) => {      
+               
+                //if (result.isConfirmed) {
+                //    Swal.fire(
+                //        'Deleted!',
+                //        'Your file has been deleted.',
+                //        'success'
+                //    )
+                //}
+
+                if (result.isConfirmed)
+                {
+                $.ajax({
+                        url: '/company/Delete/${data}',
+                        type: 'GET',
+                        data: { id: ${data} },
+                        success: function () {
+                            // Remove the row from DataTable on success
+                            //row.remove().draw();
+                            Swal.fire(
+                                'Deleted!',
+                                'Data has been deleted.',
+                                'success'
+                            ).then((a) => {
+                                location.reload();
+                            });
+                          
+                        },
+                        error: function () {
+                            // Handle error if needed
+                            Swal.fire(
+                                'Error!',
+                                'Failed to delete data.',
+                                'error'
+                            );
+                        }
+                    });
+                    }
+
+            });
+            return false;
                                     }
                            </script>`
                 }
