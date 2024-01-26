8be47f03cd38e87bc81b69943458491bae814abc
Nalla Keerthi <kumars963@yahoo.com>
2024-01-26 10:59:33 -0800

    InvoiceCartIController

---
+++ ShoppingCart/Controllers/InvoiceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using ShoppingCart.Models;
+using ShoppingCart.Repository;
+
+namespace ShoppingCart.Controllers
+{
+	public class InvoiceController : Controller
+	{
+		private readonly IRepository<Invoice> _repo;
+		private readonly IToastNotification _toastNotification;
+		public InvoiceController(IRepository<Invoice> repo, IToastNotification toastNotification)
+		{
+			_repo = repo;
+			_toastNotification = toastNotification;
+		}
+		public IActionResult Index()
+		{
+			var data = _repo.GetAll();
+			return View(data);
+			
+		}
+
+		public IActionResult GetInvoice()
+		{
+			return View();
+		}
+	}
+}
--- ShoppingCart/wwwroot/js/Company.js
+++ ShoppingCart/wwwroot/js/Company.js
@@ -1,5 +1,48 @@
-﻿$(document).ready(function () {
+﻿function deleteCompany(companyId) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
 
+
+        if (result.isConfirmed) {
+            $.ajax({
+                url: '/company/delete/' + companyId,
+                type: 'GET',
+                success: function () {
+                    // Remove the row from DataTable on success
+                    //row.remove().draw();
+                    Swal.fire(
+                        'Deleted!',
+                        'Data has been deleted.',
+                        'success'
+                    ).then((a) => {
+                        location.reload();
+                    });
+
+                },
+                error: function () {
+                    // Handle error if needed
+                    Swal.fire(
+                        'Error!',
+                        'Failed to delete data.',
+                        'error'
+                    );
+                }
+            });
+        }
+
+    });
+    return false;
+}
+
+
+$(document).ready(function () {
     $('#myTable').DataTable({
         "ajax": { url: '/Company/GetAllCompany' },
         "columns": [
@@ -22,66 +65,12 @@
                         <i class="bi bi-pencil-square"></i>Edit
                         </a>
 
-                      <a href="/company/Delete/${data}" onclick="return myFunction() ;"
+                      <a href="#" onclick="return deleteCompany(${data});"
                       class="btn btn-danger mx-2">
                     <i class="bi bi-trash-fill"></i>Delete
                     </a>
                     </div>
-                            </div>
-                            <script>
-                          function myFunction() {
-                           
-                                    Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {      
-               
-                //if (result.isConfirmed) {
-                //    Swal.fire(
-                //        'Deleted!',
-                //        'Your file has been deleted.',
-                //        'success'
-                //    )
-                //}
-
-                if (result.isConfirmed)
-                {
-                $.ajax({
-                        url: '/company/Delete/${data}',
-                        type: 'GET',
-                        data: { ${data} },
-                        success: function () {
-                            // Remove the row from DataTable on success
-                            //row.remove().draw();
-                            Swal.fire(
-                                'Deleted!',
-                                'Data has been deleted.',
-                                'success'
-                            ).then((a) => {
-                                location.reload();
-                            });
-                          
-                        },
-                        error: function () {
-                            // Handle error if needed
-                            Swal.fire(
-                                'Error!',
-                                'Failed to delete data.',
-                                'error'
-                            );
-                        }
-                    });
-                    }
-
-            });
-            return false;
-                                    }
-                           </script>`
+                            </div>`
                 }
             },
         ],
